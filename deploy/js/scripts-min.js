67494ee0c27b20f0230c5ac9cb8836dc659e446b
kathyvychung <63378621+kathyvychung@users.noreply.github.com>
2021-03-18 19:13:49 -0400

    delete

--- deploy/js/scripts-min.js
+++
@@ -1,1 +0,0 @@
-function fade2Content(){console.log("fade2Content called"),document.body.classList.remove("blackScreen")}
--- dev/js/scripts.js
+++
@@ -1,6 +0,0 @@
-function fade2Content() {
-    console.log('fade2Content called');
-  
-    //the dom
-    document.body.classList.remove("blackScreen");
-  }
