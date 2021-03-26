03b036c3dd5981e31c1244e09ac5c78b4f9b1fa6
kathyvychung <63378621+kathyvychung@users.noreply.github.com>
2021-03-26 01:45:45 -0400

    sound

--- js/main.js
+++ js/main.js
@@ -84,39 +84,204 @@ virObj.addEventListener('click', function (evt) {
 });
 
 function userPicked(whichOne) {
-  console.log('userPicked called: ' + whichOne);
+  // console.log('userPicked called: ' + whichOne);
   switch (whichOne) {
     case 'Ari':
-      // zDisplayObj.src = 'img/ariesconstellations.png';
+      zDisplayObj.src = 'img/ariesconstellations.png';
       // display title
       zTitleObj.innerHTML = 'The Ram';
       // display date range
-      zDateRangeObj.innerHTML = '##-Month to ##-Month';
+      zDateRangeObj.innerHTML = 'March 21 - April 19';
       // display description
-      zMoreInfo.innerHTML = '<p>Pisces are cool, they swim in water.</p>';
+      zMoreInfo.innerHTML = '<p>Wow! You caught sight of the 39th largest constellation in the sky! Despite being called the Ram it really…doesn’t look like one does it? The Ram, otherwise known as the Aries, is located in the Northern Hemisphere between Pisces to its west and Taurus to its east. The best view of Aries occurs on December nights around 9 p.m. local time.</p>';
       // Show hide modal
       modal.hidden = !modal.hidden;
       // play sound
-      // stopAllAudio();
-      // bulletObj.play();
+      stopAllAudio();
+      sndAriObj.play();
       break;
-    case 'Aqu':
-      // displayObj.src = 'img/ful_pisces.png';
-      // zDisplayObj.src = 'img/sml_pisces.png';
+    case 'Tau':
+      zDisplayObj.src = 'img/taurusconstellations.png';
       // display title
-      zTitleObj.innerHTML = 'PISCES';
+      zTitleObj.innerHTML = 'The Bull';
       // display date range
-      zDateRangeObj.innerHTML = '##-Month to ##-Month';
+      zDateRangeObj.innerHTML = 'April 20 – May 20';
       // display description
-      zMoreInfo.innerHTML = '<p>Pisces are cool, they swim in water.</p>';
+      zMoreInfo.innerHTML = '<p>Bullseye! You found the 6th biggest zodiac constellation. It is the 17th largest constellation overall, taking up a 1.9% area of the sky. In the northern hemisphere, Taurus is visible during the autumn and winter time. It is best seen during the month of January. </p>';
       // Show hide modal
       modal.hidden = !modal.hidden;
       // play sound
-      // stopAllAudio();
-      // bulletObj.play();
-      break;
+      stopAllAudio();
+      sndTauObj.play();
+    break;
+    case 'Gem':
+      zDisplayObj.src = 'img/geminiconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Twins';
+      // display date range
+      zDateRangeObj.innerHTML = 'May 21 – June 21';
+      // display description
+      zMoreInfo.innerHTML = '<p>Awwww you caught sight of the Gemini Twins! They are the 8th biggest zodiac constellation. It is the 30th largest constellation overall, taking up a 1.2% area of the sky. Gemini is fairly easy to spot in the sky, even for amateur stargazers. It is located northeast of the constellation Orion and between the Taurus and Cancer constellations. Best viewing is during February. By April and May, the constellation can be seen soon after sunset in the west.</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndGemObj.play();
+    break;
+    case 'Can':
+      zDisplayObj.src = 'img/cancerconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Crab';
+      // display date range
+      zDateRangeObj.innerHTML = 'June 22–July 22';
+      // display description
+      zMoreInfo.innerHTML = '<p>Don\'t get too crabby... but it\'s the 9th biggest zodiac constellation! It is the 31st largest constellation overall, taking up a 1.2% area of the sky. Cancer lies between Leo, the lion, and Gemini, the twins. It is almost impossible to see Cancer as a crab with the naked eye or even binoculars so consider yourself lucky tonight! It looks more like a faint, upside-down Y. Cancer is visible in the Northern Hemisphere in the early spring. It can be seen in the Southern Hemisphere during autumn.</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndCanObj.play();
+    break;
+    case 'Leo':
+      zDisplayObj.src = 'img/leoconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Lion';
+      // display date range
+      zDateRangeObj.innerHTML = 'July 23 – August 22';
+      // display description
+      zMoreInfo.innerHTML = '<p>Look what we hunted! The Leo (Lion) is the 3rd biggest zodiac constellation. It is the 12th largest constellation overall, taking up a 2.3% area of the sky. Leo is a highly recognizable constellation, as it is one of the few constellations that resemble its namesake. It is fairly easy to find because the "pointer stars" of the Big Dipper point to Leo. The constellation becomes visible in the Northern Hemisphere around the spring equinox and is easily identifiable through May.</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndLeoObj.play();
+    break;
+    case 'Vir':
+      zDisplayObj.src = 'img/virconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Virgin';
+      // display date range
+      zDateRangeObj.innerHTML = 'August 23 – September 22';
+      // display description
+      zMoreInfo.innerHTML = '<p>Pleasure to meet you, young maiden! The Virgo is the largest zodiac constellation. It is also the 2nd largest constellation overall, taking up a 3.1% area of the sky. Tied to fertility and agriculture, Virgo appears to stargazers in the Northern Hemisphere during the spring and summer months and to those in the Southern Hemisphere in autumn and winter.</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndVirObj.play();
+    break;
+    case 'Lib':
+      zDisplayObj.src = 'img/libconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Balance Scale';
+      // display date range
+      zDateRangeObj.innerHTML = 'September 23 – October 23';
+      // display description
+      zMoreInfo.innerHTML = '<p>There\'s balance in this universe... haha get it? This is the 7th biggest zodiac constellation. It is the 29th largest constellation overall, taking up a 1.3% area of the sky. Libra is in the Southern Hemisphere, located between Scorpius to the east and Virgo to the west. Did you know this constellation was almost considered part of the Scorpius until Roman astronomers redrew the skyline and moved the claws of the scorpion to Libra!</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndLibObj.play();
+    break;
+    case 'Sco':
+      zDisplayObj.src = 'img/scoconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Scorpion';
+      // display date range
+      zDateRangeObj.innerHTML = 'October 24 – November 21';
+      // display description
+      zMoreInfo.innerHTML = '<p>Oh my! It\'s the Scorpion, 10th biggest zodiac constellation! It is the 33rd largest constellation overall, taking up a 1.2% area of the sky. The Scorpius constellation has intrigued people for centuries, not only for its distinctive shape, but also because it is one of the brightest constellations in the sky. Scorpius is visible during the summer time from northern hemisphere locations. It is best seen during the month of July.</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndScoObj.play();
+    break;
+    case 'Sag':
+      zDisplayObj.src = 'img/sagconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Archer';
+      // display date range
+      zDateRangeObj.innerHTML = 'November 22 – December 21';
+      // display description
+      zMoreInfo.innerHTML = '<p>We have located our target, it\'s the Archer! It\'s the 5th biggest zodiac constellation and the 15th largest constellation overall, taking up a 2.1% area of the sky! Sagittarius is at the center of the Milky Way Galaxy, and the galaxy is at its densest point as it makes its way through Sagittarius. You can especially find this constellation during the autumn time from northern hemisphere locations. It is best seen during the month of August!</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndSagObj.play();
+    break;
+    case 'Cap':
+      zDisplayObj.src = 'img/capconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Goat';
+      // display date range
+      zDateRangeObj.innerHTML = 'December 22 – January 19';
+      // display description
+      zMoreInfo.innerHTML = '<p>We found the Goat\'s Horn! The Capricorn! This constellation is the smallest of the 12 zodiac constellations. It is the 40th largest constellation in the night sky overall, taking up a 1.0% area of the celestial heavens. Capricornus is among the faintest constellations, just brighter than Cancer, but can be seen during the summer and autumn time from northern hemisphere locations. It is best seen during the month of September!</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndCapObj.play();
+    break;
+    case 'Aqu':
+      zDisplayObj.src = 'img/aquconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Water Bearer';
+      // display date range
+      zDateRangeObj.innerHTML = 'January 20 – February 18';
+      // display description
+      zMoreInfo.innerHTML = '<p>Water you doing here? It\'s the Water Bearer, the 2nd biggest zodiac constellation! It is the 10th largest constellation overall, taking up a 2.4% area of the sky. Aquarius is visible during the autumn and winter time from northern hemisphere locations. It is best seen during the month of October.</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndAquObj.play();
+    break;
+    case 'Pis':
+      zDisplayObj.src = 'img/pisconstellations.png';
+      // display title
+      zTitleObj.innerHTML = 'The Fish';
+      // display date range
+      zDateRangeObj.innerHTML = 'February 19 – March 20';
+      // display description
+      zMoreInfo.innerHTML = '<p>Look at this good catch, it\'s the Pisces Fish! This constellation is 4th biggest zodiac constellation! It is the 14th largest constellation overall, taking up a 2.2% area of the sky. Pisces is visible in the northern hemisphere between late summer and winter. It is best seen during the month of November.</p>';
+      // Show hide modal
+      modal.hidden = !modal.hidden;
+      // play sound
+      stopAllAudio();
+      sndPisObj.play();
+    break;
+    
   } 
 }
+
+function stopAllAudio() {
+  sndAriObj.pause();
+  sndAriObj.currentTime = 0;
+
+  // haloObj.pause();
+  // haloObj.currentTime = 0;
+
+  // waterObj.pause();
+  // waterObj.currentTime = 0;
+}
+
+//sound controls
+const sndAriObj = document.getElementById('sndAries');
+const sndCanObj = document.getElementById('sndCancer');
+const sndCapObj = document.getElementById('sndCap');
+const sndGemObj = document.getElementById('sndGem');
+const sndLeoObj = document.getElementById('sndLeo');
+const sndLibObj = document.getElementById('sndLib');
+const sndPisObj = document.getElementById('sndPis');
+const sndSagObj = document.getElementById('sndSag');
+const sndScoObj = document.getElementById('sndSco');
+const sndTauObj = document.getElementById('sndTau');
+const sndVirObj = document.getElementById('sndVir');
+const sndAquObj = document.getElementById('sndAqu');
 
 
 //calendar input code
@@ -143,29 +308,29 @@ const userFirstName =  document.getElementById('fname').value;
   let AstroSign = "";
 
   if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
-    AstroSign = "cap";
+    AstroSign = "Cap";
   } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
-    AstroSign = "sag";
+    AstroSign = "Sag";
   } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
-    AstroSign = "sco";
+    AstroSign = "Sco";
   } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
-    AstroSign = "lib";
+    AstroSign = "Lib";
   } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
-    AstroSign = "vir";
+    AstroSign = "Vir";
   } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
-    AstroSign = "leo";
+    AstroSign = "Leo";
   } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
-    AstroSign = "can";
+    AstroSign = "Can";
   } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
-    AstroSign = "gem";
+    AstroSign = "Gem";
   } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
-    AstroSign = "tau";
+    AstroSign = "Tau";
   } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
-    AstroSign = "ari";
+    AstroSign = "Ari";
   } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
-    AstroSign = "pis";
+    AstroSign = "Pis";
   } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
-    AstroSign = "aqu";
+    AstroSign = "Aqu";
   }
 
   console.log('AstroSign is ' + AstroSign);
@@ -179,7 +344,7 @@ const modal = document.getElementById('modal');
 const closeModalObj = document.getElementById('btn-modal-close');
 closeModalObj.addEventListener('click', function() {
   modal.hidden = !modal.hidden;
- 
+  stopAllAudio();
 });
 
 // Link modal element/objects to JS
